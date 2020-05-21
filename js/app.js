8a32dc1c820046e850bf03d2388285a105ccb5e6
sebastian <sebastian.lorca@wic.uchile.cl>
2020-05-21 01:15:38 -0400

    propuesta de valor

--- js/app.js
+++ js/app.js
@@ -1,38 +1,56 @@
 $(document).ready(function(){
 
     //scroll nav color
-    $(window).on('scroll', function(){
-        if($(window).scrollTop()){
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
             $('header').addClass('primary-bg');
         }
-        else
-        {
+        else {
             $('header').removeClass('primary-bg');
         }
     })
 
     //menu responsive
-    $('.menu-toggle').click(function(){
+    $('.menu-toggle').click(function () {
         $('nav').toggleClass('active')
     })
-    $('nav ul li a').click(function(){
+    $('nav ul li a').click(function () {
         $('nav').toggleClass('active')
     })
 
     //hamburger to x
-    $('.main-nav-toggle').click(function(){
+    $('.main-nav-toggle').click(function () {
         $('.main-nav-toggle').toggleClass('active-menu')
     })
-    $('nav ul li a').click(function(){
+    $('nav ul li a').click(function () {
         $('.main-nav-toggle').toggleClass('active-menu')
     })
 
-    // $('a').on('click', function(e){
-    //     e.preventDefault();
-    //   if ( $(this).hasClass('active-menu') ) {
-    //     $( this ).removeClass( 'active-menu' );
-    //   } else {
-    //     $( this ).addClass( 'active-menu' );
-    //   }
-    // });
+    // owl-carousel
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+          "<i class='fa fa-caret-left'></i>",
+          "<i class='fa fa-caret-right'></i>"
+        ],
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          700: {
+            items: 2
+          },
+          900: {
+            items: 3
+          },
+          1200: {
+            items: 4
+          }
+        }
+      })
+
 })
